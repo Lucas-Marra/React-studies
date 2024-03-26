9e89ed1f6ccf33f59e89b67ad4d032546d4a1b9c
Lucas Rebello <lucas.rebello@alura.com.br>
2024-03-26 09:25:39 -0300

    style(alura-react-voll-adm): add style to table

--- alura-react-voll-adm/src/Tabela/index.tsx
+++ alura-react-voll-adm/src/Tabela/index.tsx
@@ -5,9 +5,31 @@ import {
     TableHead,
     Table,
     TableRow,
-    TableBody
+    TableBody,
+    tableCellClasses
 } from '@mui/material'
 import QueryData from '../types/QueryData'
+import styled from 'styled-components'
+
+const StyledCell = styled(TableCell)(() => ({
+    [`&.${tableCellClasses.head}`]: {
+        color: 'var(--azul-escuro)',
+        fontSize: 18,
+        fontWeight: 700,
+        fontFamily: 'var(--fonte-principal)'
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 16,
+        fontFamily: 'var(--fonte-principal)'
+    }
+}))
+
+const StyledLine = styled(TableRow)(() => ({
+    [`&:nth-of-type(odd)`]: {
+        backgroundColor: 'var(--cinza-claro)',
+        align: 'right'
+    }
+}))
 
 export default function DataTable({ querys }: { querys: QueryData[] | null }) {
     return (
@@ -16,27 +38,27 @@ export default function DataTable({ querys }: { querys: QueryData[] | null }) {
                 <Table sx={{ minWidth: 700 }} aria-label="tabela customizada">
                     <TableHead>
                         <TableRow>
-                            <TableCell>Data</TableCell>
-                            <TableCell>Horário</TableCell>
-                            <TableCell>Profissional</TableCell>
-                            <TableCell>Especialidade</TableCell>
-                            <TableCell>Paciente</TableCell>
-                            <TableCell>Modalidade</TableCell>
+                            <StyledCell>Data</StyledCell>
+                            <StyledCell>Horário</StyledCell>
+                            <StyledCell>Profissional</StyledCell>
+                            <StyledCell>Especialidade</StyledCell>
+                            <StyledCell>Paciente</StyledCell>
+                            <StyledCell>Modalidade</StyledCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {querys?.map((line) => {
                             return (
-                                <TableRow>
-                                    <TableCell component="th" scope="row">
+                                <StyledLine>
+                                    <StyledCell component="th" scope="row">
                                         {new Date(line.date).toLocaleDateString()}
-                                    </TableCell>
-                                    <TableCell>{line.time}</TableCell>
-                                    <TableCell>{line.professional[0].name}</TableCell>
-                                    <TableCell>{line.professional[0].specialty}</TableCell>
-                                    <TableCell>{line.patient}</TableCell>
-                                    <TableCell>{line.modality}</TableCell>
-                                </TableRow>
+                                    </StyledCell>
+                                    <StyledCell>{line.time}</StyledCell>
+                                    <StyledCell>{line.professional[0].name}</StyledCell>
+                                    <StyledCell>{line.professional[0].specialty}</StyledCell>
+                                    <StyledCell>{line.patient}</StyledCell>
+                                    <StyledCell>{line.modality}</StyledCell>
+                                </StyledLine>
                             )
                         })}
                     </TableBody>
